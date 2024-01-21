b55bd450338f090bcdd676ef6f239b07984c24a7
Kacey Chang <d9453302@gmail.com>
2024-01-20 16:46:43 -0800

    Add Heatmap Plot

---
+++ Heatmap_Plot_Program.R
@@ -0,0 +1,53 @@
+rm(list = ls())
+library("dplyr")
+library("tidyr")
+library("ggplot2")
+#library("ggthemes")
+
+
+load(file = "matrix_Go_NoGo.RData")
+
+#matrix to array
+mut_preprocess <- as.data.frame(matrix_Go_NoGo)
+row_no <- nrow(mut_preprocess)
+col_no <- ncol(mut_preprocess)
+colnames(mut_preprocess) <- factor(paste0("rc", 0:(col_no - 1)))
+mut_preprocess_tidy <- mut_preprocess %>%  gather(key = "rc", value = "value")
+mut_preprocess_tidy$rt <- rep(factor(paste0("rt", 0:(row_no - 1))), col_no)
+
+#Plot the graph preprocess
+mut <- mut_preprocess_tidy#array_Go_NoG0
+mut <- as.data.frame(mut)
+names(mut) <- c("rc", "decision", "rt")
+
+mut$rc <- factor(mut$rc, levels = unique(mut$rc))
+mut$rt <- factor(mut$rt, levels = unique(mut$rt))
+mut$decision <- factor(mut$decision, levels = unique(mut$decision))
+
+gg <- ggplot(mut, aes(x = rc, y = rt, fill = decision)) +
+  geom_tile(color= "white", size = 0.5, show.legend = FALSE) +
+  #scale_fill_manual(values = c("#fffa8c", "#6dc700", "#ff0000")) +
+  scale_fill_manual(values = c("#ff0000", "#fffa8c", "#6dc700")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12), axis.text.y = element_text(size = 12)) +
+  #scale_x_discrete(position = "top") +
+  #scale_y_discrete(name = "", labels = row_labels) +
+  labs(x=NULL, y=NULL) +
+  geom_text(aes(label=decision), size=2)
+
+show(gg)
+
+#Reverse
+mut$rt <- rev(mut$rt)
+row_labels <- rev(levels(mut$rt))
+gg <- ggplot(mut, aes(x = rc, y = rt, fill = decision)) +
+  geom_tile(color= "white", size = 0.5, show.legend = FALSE) +
+  #scale_fill_manual(values = c("#fffa8c", "#6dc700", "#ff0000")) +
+  scale_fill_manual(values = c("#ff0000", "#fffa8c", "#6dc700")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 0.5, size = 12), axis.text.y = element_text(size = 12)) +
+  scale_x_discrete(position = "top") +
+  scale_y_discrete(name = "", labels = row_labels) +
+  labs(x=NULL, y=NULL) +
+  geom_text(aes(label=decision), size=2)
+
+show(gg)
+
